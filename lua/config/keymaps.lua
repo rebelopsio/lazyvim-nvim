2af13b7a662dc3f9def1e1c8992665b9ab3362c2
Stephen Morgan <steve@rebelops.io>
2023-07-23 09:36:47 -0400

    feat: add some keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,14 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<C-a>", ":Neotree focus<CR>", { desc = "Neotree focus", remap = true })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move block down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move block up" })
+
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Better J" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page jumping down" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page jumping up" })
+
+vim.keymap.set("n", "n", "nzzzv", { desc = "Better search jumping down" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Better search jumping up" })
+
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Better paste" })
