81e5bb863d2d54e58fbfabee1ca96fe35139043d
Stephen Morgan <steve@rebelops.io>
2023-08-04 08:31:43 -0400

    feat: add yaml companion

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -1,5 +1,14 @@
 return {
   {
     "xiyaowong/transparent.nvim",
+    lazy = false,
+    config = function()
+      vim.g.transparent_groups = vim.list_extend(
+        vim.g.transparent_groups or {},
+        vim.tbl_map(function(v)
+          return v.hl_group
+        end, vim.tbl_values(require("notify.config").highlights))
+      )
+    end,
   },
 }
